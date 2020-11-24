96fb19a36852eb7a6ebfad53090a7021ec29ad32
Phil Wang <lucidrains@gmail.com>
2020-11-23 22:03:52 -0800

    finish pixel propagation module (PPM)

---
+++ pixel_level_contrastive_learning/pixel_level_contrastive_learning.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, einsum
+
+class PPM(nn.Module):
+    def __init__(
+        self,
+        *,
+        chan,
+        num_layers = 1,
+        gamma = 2):
+        super().__init__()
+        self.gamma = gamma
+
+        if num_layers == 0:
+            self.transform_net = nn.Identity()
+        elif num_layers == 1:
+            self.transform_net = nn.Conv2d(chan, chan, 1)
+        elif num_layers == 2:
+            self.transform_net = nn.Sequential(
+                nn.Conv2d(chan, chan, 1),
+                nn.BatchNorm2d(chan),
+                nn.ReLU(),
+                nn.Conv2d(chan, chan, 1)
+            )
+        else:
+            raise ValueError('num_layers must be one of 0, 1, or 2')
+
+    def forward(self, x):
+        xi = x[:, :, :, :, None, None]
+        xj = x[:, :, None, None, :, :]
+        similarity = F.relu(F.cosine_similarity(xi, xj, dim = 1)) ** self.gamma
+
+        transform_out = self.transform_net(x)
+        out = einsum('b x y h w, b c h w -> b c x y', similarity, transform_out)
+        return out
